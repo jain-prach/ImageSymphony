dcfe35a763c37dcda1c1b9ed66078de8b78cf315
jain-prach <prachijain8917@gmail.com>
2024-04-28 11:11:25 +0530

    trying fixes

--- frontend/src/components/Module2/Module2.jsx
+++ frontend/src/components/Module2/Module2.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Module2.css';
 
 const Module2 = () => {
+    const [processedNoise, setProcessedNoise] = useState(null);
+
+    useEffect(() => {
+        const fetchProcessedNoise = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/output_image_processing/noise_display');
+                
+                if (!response.ok) {
+                    throw new Error('Failed to fetch processed noise data');
+                }
+
+                const data = await response.json();
+                setProcessedNoise(data.processed_noise);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchProcessedNoise();
+    }, []);
+
+    const handleImageDownload = () => {
+        if (processedNoise) {
+            const downloadLink = document.createElement('a');
+            downloadLink.href = `data:image/png;base64,${processedNoise}`;
+            downloadLink.download = 'processed_noise_image.png';
+            downloadLink.click();
+        }
+    };
+
     return (
-        <div>Module2</div>
-    )
+        <div className="processed_noise_display">
+            <h2>Display Processed Noise Image</h2>
+            {processedNoise && (
+                <div>
+                    <h3>Processed Noise Image</h3>
+                    <img src={`data:image/png;base64,${processedNoise}`} alt="Processed Noise" />
+                    <button onClick={handleImageDownload}>Download Image</button>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Module2;
